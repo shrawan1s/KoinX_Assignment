5f81ab29e3d1c90847ffbea4be313d9830d58c91
Shrawan <shrawanthakur0962@gmail.com>
2024-06-26 01:37:02 +0530

    Initial commit setup project structure and basic files

---
+++ src/db.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/crypto', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => {
+    console.error('Failed to connect to MongoDB', err);
+});
+
+export default connect
